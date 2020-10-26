1207a24b19d0371cd93021b68133243ff4285374
Tristan-Voiron <tristan.voiron@gmail.com>
2020-10-27 00:05:03 +0100

    Details Screen is working

--- src/Screens/PlayerDetails.tsx
+++ src/Screens/PlayerDetails.tsx
@@ -1,4 +1,4 @@
-import { Clipboard, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Clipboard, FlatList, SafeAreaView, ScrollView, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, {Component} from 'react';
 
 import ErrorIndicator from '../Components/ErrorIndicator';
@@ -60,18 +60,31 @@ export default class PlayerDetails extends Component<Props, State> {
       )
     }
 
+    let {data} = this.state;
+    let {stats} = data;
+    let isGuardian: boolean = false;
+    let errorLeadToGoal: number = 0;
+
+    if (this.state.data.ultraPosition === 10) {
+      isGuardian = true;
+    } else {
+      for (var i = 0; i < stats.matches.length; i++) {
+        errorLeadToGoal += stats.matches[i].stats.error_lead_to_goal;
+      }
+    }
+
     return (
       <SafeAreaView style={styles.container}>
         <View style={styles.subContainer}>
           <View style={styles.playerInfo}>
             <View style={styles.columnContainer}>
-              <Text style={styles.playerInfoText}>N° {this.state.data.jerseyNum}</Text>
-              <Text style={styles.playerInfoText}>{this.state.data.club} - {getPosition(this.state.data.ultraPosition)}</Text>
+              <Text style={styles.playerInfoText}>N° {data.jerseyNum}</Text>
+              <Text style={styles.playerInfoText}>{data.club} - {getPosition(data.ultraPosition)}</Text>
             </View>
             <TouchableOpacity 
               onPress={() => 
               (
-                Clipboard.setString(this.state.data.twitter),
+                Clipboard.setString(data.twitter),
                 showMessage({
                   message: "Copié dans le presse-papier",
                 })
@@ -90,61 +103,213 @@ export default class PlayerDetails extends Component<Props, State> {
             <View style={styles.rowContainer}>
               <View style={styles.columnContainer}>
                 <Text style={styles.title}>Note Moy.</Text>
-                <Text style={styles.detail}>{this.state.data.stats.avgRate}</Text>
+                <Text style={styles.detail}>{stats.avgRate}</Text>
               </View>
               {
-                this.state.data.ultraPosition === 10 ? 
+                isGuardian ? 
                 (
                   <View style={styles.columnContainer}>
                     <Text style={styles.title}>Clean Sheet</Text>
-                    <Text style={styles.detail}>{this.state.data.stats.sumCleanSheet}</Text>
+                    <Text style={styles.detail}>{stats.sumCleanSheet}</Text>
                   </View>
                 )
                 : 
                 (
                   <View style={styles.columnContainer}>
                     <Text style={styles.title}>Buts (pén)</Text>
-                <Text style={styles.detail}>{this.state.data.stats.sumGoals} ({this.state.data.stats.sumPenalties})</Text>
+                <Text style={styles.detail}>{stats.sumGoals} ({stats.sumPenalties})</Text>
                   </View>
                 )
               }
               <View style={styles.columnContainer}>
                 <Text style={styles.title}>Titulaire (remp.)</Text>
-                <Text style={styles.detail}>{this.state.data.stats.appearances.starter} ({this.state.data.stats.appearances.standIn})</Text>
+                <Text style={styles.detail}>{stats.appearances.starter} ({stats.appearances.standIn})</Text>
               </View>
             </View>
             <View style={styles.rowContainer}>
               <View style={styles.columnContainer}>
                 <Text style={styles.title}>Cote</Text>
-                <Text style={styles.detail}>{this.state.data.quotation}</Text>
+                <Text style={styles.detail}>{data.quotation}</Text>
               </View>
               {
-                this.state.data.ultraPosition === 10 ? 
+                isGuardian ? 
                 (
                   <View style={styles.columnContainer}>
                     <Text style={styles.title}>% Sauvés</Text>
-                    <Text style={styles.detail}>{this.state.data.stats.percentageSaveShot}</Text>
+                    <Text style={styles.detail}>{stats.percentageSaveShot}</Text>
                   </View>
                 )
                 : 
                 (
                   <View style={styles.columnContainer}>
                     <Text style={styles.title}>Passes dé.</Text>
-                    <Text style={styles.detail}>{this.state.data.stats.sumGoalAssist}</Text>
+                    <Text style={styles.detail}>{stats.sumGoalAssist}</Text>
                   </View>
                 )
               }
               <View style={styles.columnContainer}>
                 <Text style={styles.title}>Cartons</Text>
                 <View style={styles.rowContainer}>
-                  <Text style={[styles.detail, {backgroundColor: 'red'}]}>{this.state.data.stats.sumRedCard}</Text>
-                  <Text style={[styles.detail, {backgroundColor: 'yellow'}]}>{this.state.data.stats.sumYellowCard}</Text>
+                  <Text style={[styles.detail, {backgroundColor: 'red'}]}>{stats.sumRedCard}</Text>
+                  <Text style={[styles.detail, {backgroundColor: 'yellow'}]}>{stats.sumYellowCard}</Text>
                 </View>
               </View>
             </View>
           </View>
           
           <ScrollView>
+            <View style={styles.playerScore}>
+              <Text style={styles.playerInfoText}>En Forme ?</Text>
+              <FlatList 
+                horizontal={true}
+                data={stats.matches}
+                renderItem={({ item }) =>
+                  (
+                    stats.lastFiveRate.hasOwnProperty(item.day) ?
+                      <View style={styles.rate}>
+                        <Text>J.{item.day}</Text>
+                        <Text>{item.info.rate}</Text>
+                      </View>
+                    :
+                      null
+                  )
+                }
+                keyExtractor={item => item.id}
+              />
+            </View>
+
+            {
+              isGuardian ? 
+              (
+                <View style={styles.playerScore}>
+                  <Text style={styles.playerInfoText}>Efficace ?</Text>
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.title}>But encaissés par match</Text>
+                    <Text style={styles.title}>{stats.goalsConcededByMatch}</Text>
+                  </View>
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.title}>Arrêts réalisés</Text>
+                    <Text style={styles.title}>{stats.sumSaves}</Text>
+                  </View>
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.title}>Parades</Text>
+                    <Text style={styles.title}>{stats.sumDeflect}</Text>
+                  </View>
+                  <View style={styles.rowContainer}>
+                    <Text style={styles.title}>Pénaltys sauvés</Text>
+                    <Text style={styles.title}>{stats.sumPenaltySave}</Text>
+                  </View>
+                </View>
+              )
+              : 
+              (
+                <View>
+                  <View style={styles.playerScore}>
+                    <Text style={styles.playerInfoText}>Efficace ?</Text>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Dribbles réussies par match</Text>
+                      <Text style={styles.title}>{stats.wonContestByMatch} ({stats.percentageWonContest}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Duels remportés par match</Text>
+                      <Text style={styles.title}>{stats.wonDuelByMatch} ({stats.percentageWonDuel}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Pertes de balles par match</Text>
+                      <Text style={styles.title}>{stats.lostBallByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Fautes commises par match</Text>
+                      <Text style={styles.title}>{stats.foulsByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Fautes subies par match</Text>
+                      <Text style={styles.title}>{stats.foulsEnduredByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Tirs cadrés par match</Text>
+                      <Text style={styles.title}>{stats.shotOnTargetByMatch} ({stats.percentageShotOnTarget}%)</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.playerScore}>
+                    <Text style={styles.playerInfoText}>Il Plante ?</Text>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Buts (pén.)</Text>
+                      <Text style={styles.title}>{stats.sumGoals} ({stats.sumPenalties})</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Fréquence de but (min.)</Text>
+                      <Text style={styles.title}>{stats.minutesByGoal}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Buts par match</Text>
+                      <Text style={styles.title}>{stats.goalByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Tirs par match</Text>
+                      <Text style={styles.title}>{stats.shotByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Grosses occasions ratées</Text>
+                      <Text style={styles.title}>{stats.sumBigChanceMissed}</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.playerScore}>
+                    <Text style={styles.playerInfoText}>Solide ?</Text>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Interceptions par match</Text>
+                      <Text style={styles.title}>{stats.interceptByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Tacles par match</Text>
+                      <Text style={styles.title}>{stats.tackleByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Buts encaissés par match</Text>
+                      <Text style={styles.title}>{stats.goalsConcededByMatch}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Erreurs qui amènent un but</Text>
+                      <Text style={styles.title}>{errorLeadToGoal}</Text>
+                    </View>
+                  </View>
+
+                  <View style={styles.playerScore}>
+                    <Text style={styles.playerInfoText}>Un As de la Passe ?</Text>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Passes décisives</Text>
+                      <Text style={styles.title}>{stats.sumGoalAssist}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Grosses occasions créées</Text>
+                      <Text style={styles.title}>{stats.sumBigChanceCreated}</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Passes réussies par match</Text>
+                      <Text style={styles.title}>{stats.succeedPassByMatch} ({stats.percentageSucceedPass}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Passes réussies dans le camp adverse par match</Text>
+                      <Text style={styles.title}>{stats.succeedPassFwdZoneByMatch} ({stats.percentageAccurateFwdZone}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Passes réussies dans son camp par match</Text>
+                      <Text style={styles.title}>{stats.succeedPassBackZoneByMatch} ({stats.percentageAccuratePassBackZone}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Passes longues réussies par match</Text>
+                      <Text style={styles.title}>{stats.succeedLongPassByMatch} ({stats.percentageAccurateLongPass}%)</Text>
+                    </View>
+                    <View style={styles.rowContainer}>
+                      <Text style={styles.title}>Centres réussies par match</Text>
+                      <Text style={styles.title}>{stats.succeedCrossByMatch} ({stats.percentageCrossSuccess}%)</Text>
+                    </View>
+                  </View>
+                </View>
+              )
+            }
 
           </ScrollView>
         </View>
@@ -189,7 +354,6 @@ const styles = StyleSheet.create({
   iconStyle: {
     marginHorizontal: 10,
   },
-
   playerScore: {
     borderWidth: 0.2,
     padding: 10,
@@ -200,6 +364,7 @@ const styles = StyleSheet.create({
   title: {
     marginHorizontal: 10,
     fontSize: 14,
+    flexShrink: 1,
   },
   detail: {
     marginHorizontal: 10,
@@ -207,4 +372,13 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     alignSelf: 'center',
   },
+
+  rate: {
+    borderRadius: 30,
+    backgroundColor: '#00acee',
+    marginHorizontal: 10,
+    padding: 10,
+    marginVertical: 10,
+    alignItems: 'center',
+  }
 });
